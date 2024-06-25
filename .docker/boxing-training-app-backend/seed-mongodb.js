8b702b8148058251ee514b07ffd351eb05c69433
caesar-kaho <kwok.kaho@outlook.com>
2024-06-25 19:52:21 +0800

    updated the seed-mongodb.js

--- .docker/boxing-training-app-backend/seed-mongodb.js
+++ .docker/boxing-training-app-backend/seed-mongodb.js
@@ -3,24 +3,166 @@ print("Seeding the collection 'users' in the 'boxing-db' db");
 db.users.insert([
     {
         name: "name01",
-        email: "name01@test.com",
+        logEntites:[
+            "log01","log02"
+        ]
     },
     {
         name: "name02",
-        email: "name02@test.com",
+        logEntites:[
+            "log01","log02"
+        ]
     },
     {
         name: "name03",
-        email: "name03@test.com",
+        logEntites:[
+            "log01","log02"
+        ]
     },
     {
         name: "name04",
-        email: "name04@test.com",
+        logEntites:[
+            "log01","log02"
+        ]
     },
     {
         name: "name05",
-        email: "name05@test.com",
+        logEntites:[
+            "log01","log02"
+        ]
     },
 ]);
 
+db.logs.insert([
+    {
+        logId:"log01",
+        dateTime: "20240625",
+        workouts:[
+            "wo01","wo02"
+        ]
+    },
+    {
+        logId:"log02",
+        dateTime: "20240626",
+        workouts:[
+            "wo01","wo02"
+        ]
+    },
+])
+
+db.workouts.insert([
+    {
+        workoutId:"wo01",
+        activitiesId: "2",
+        workouts:[
+            "wo01","wo02"
+        ]
+    },
+    {
+        logId:"log02",
+        dateTime: "20240626",
+        workouts:[
+            "wo01","wo02"
+        ]
+    },
+])
+
+db.skippings.insert([
+    {
+        skippingId: "s01",
+        roundComp: 0,
+        totalRound:3,
+        totalTime:540
+    },
+    {
+        skippingId: "s02",
+        roundComp: 0,
+        totalRound:3,
+        totalTime:540
+    },
+    {
+        skippingId: "s03",
+        roundComp: 0,
+        totalRound:3,
+        totalTime:540
+    },
+    {
+        skippingId: "s04",
+        roundComp: 0,
+        totalRound:3,
+        totalTime:540
+    },
+    {
+        skippingId: "s05",
+        roundComp: 0,
+        totalRound:3,
+        totalTime:540
+    },
+]);
+
+db.sandbags.insert([
+    {
+        sandbagId: "sb01",
+        roundComp: 0,
+        totalRound:3,
+        totalTime:540
+    },
+    {
+        sandbagId: "sb02",
+        roundComp: 0,
+        totalRound:3,
+        totalTime:540
+    },
+    {
+        sandbagId: "sb03",
+        roundComp: 0,
+        totalRound:3,
+        totalTime:540
+    },
+    {
+        sandbagId: "sb04",
+        roundComp: 0,
+        totalRound:3,
+        totalTime:540
+    },
+    {
+        sandbagId: "sb05",
+        roundComp: 0,
+        totalRound:3,
+        totalTime:540
+    },
+]);
+
+db.shadowBoxings.insert([
+    {
+        shadowBoxingId: "sb01",
+        roundComp: 0,
+        totalRound:3,
+        totalTime:540
+    },
+    {
+        shadowBoxingId: "sb02",
+        roundComp: 0,
+        totalRound:3,
+        totalTime:540
+    },
+    {
+        shadowBoxingId: "sb03",
+        roundComp: 0,
+        totalRound:3,
+        totalTime:540
+    },
+    {
+        shadowBoxingId: "sb04",
+        roundComp: 0,
+        totalRound:3,
+        totalTime:540
+    },
+    {
+        shadowBoxingId: "sb05",
+        roundComp: 0,
+        totalRound:3,
+        totalTime:540
+    },
+]);
 print("Completed seeding MongoDB!");
